39d642020570222f008eb65aff2e7f8890cae86f
DevMahbubHussain <devmahbubmamun@gmail.com>
2025-09-01 16:56:34 +0600

    Task1 & Task2 & Task3 Completed

--- README.md
+++ README.md
@@ -10,11 +10,34 @@ This project uses **Pulumi** with **Python** to create a basic AWS networking se
 - NAT Gateway
 - Public and Private Route Tables
 
-Task1:
-- `images/task1/pulumi-preview.png`
-- `images/task1/pulumi-up.png`
-- `images/task1/pulumi-stack.png`
-- `images/task1/stackjson.png`
+Task1: Create a Secure VPC
+
+![pulumi-preview](images/task1/pulumi-preview.png)
+![pulumi-up](images/task1/pulumi-up.png)
+![pulumi-stack](images/task1/pulumi-stack.png)
+![stackjson](images/task1/stackjson.png)
+
+
+### Task 2 — Bastion Host in Public Subnet
+
+- EC2 in the public subnet with a public IPv4
+- Security Group (bastion-sg): allow inbound TCP/22 only from your public IP.
+- System hardening basics via User Data
+
+Task2: Bastion Host in Public Subnet
+
+![pulumi-stack](images/task2/pulumi-stack.png)
+![SSH-session](images/task2/SSH-session.png)
+
+
+### Task 3 — Private EC2 Instance
+
+- EC2 in private subnet, SG (app-sg) must allow inbound SSH (22) only from bastion-sg (security group reference), not from the internet.
+
+Task3: Private EC2 Instance
+
+![SSH-session](images/task3/ssh.png)
+![SSH-session](images/task3/sshs.png)
 
 
 
@@ -33,8 +56,6 @@ pulumi config set aws:region ap-southeast-1
 pulumi config set vpcCidr 10.0.0.0/16
 pulumi config set publicSubnetCidr 10.0.1.0/24
 pulumi config set privateSubnetCidr 10.0.2.0/24
-
-
-
+pulumi consfig set instanceType: "t2.micro"
 
 
--- __main__.py
+++ __main__.py
@@ -2,12 +2,17 @@
 
 import pulumi
 from pulumi_aws import ec2
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+# Task 1 — Create a Secure VPC
 
 # Get value from Config file
 config = pulumi.Config()
 vpc_cidr = config.require("vpcCidr")
 public_subnet_cidr = config.require("publicSubnetCidr")
 private_subnet_cidr = config.require("privateSubnetCidr")
+instance_type = config.require("instanceType")
 
 # Create vpc
 vpc = ec2.Vpc(
@@ -96,3 +101,153 @@ pulumi.export("internat_gatway_id", igw.id)
 pulumi.export("public_route_table_id", public_route_table.id)
 pulumi.export("private_route_table_id", private_route_table.id)
 pulumi.export("nat_gateway_id", nat_gateway.id)
+
+
+# Task 2 — Bastion Host in Public Subnet
+# Generate RSA key pair
+private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+private_key_pem = private_key.private_bytes(
+    encoding=serialization.Encoding.PEM,
+    format=serialization.PrivateFormat.TraditionalOpenSSL,
+    encryption_algorithm=serialization.NoEncryption(),
+)
+public_key = (
+    private_key.public_key()
+    .public_bytes(
+        encoding=serialization.Encoding.OpenSSH,
+        format=serialization.PublicFormat.OpenSSH,
+    )
+    .decode()
+)
+
+# Create KeyPair in AWS using the generated public key
+key_pair = ec2.KeyPair(
+    "dynamic-keypair",
+    key_name="dynamic-keypair",
+    public_key=public_key,
+    tags={"Name": "dynamic-keypair", "Environment": "dev"},
+)
+
+# User data for hardening
+user_data = f"""#!/bin/bash
+# Create non-root user
+useradd -m -s /bin/bash ops
+mkdir -p /home/ops/.ssh
+echo "{public_key}" > /home/ops/.ssh/authorized_keys
+chown -R ops:ops /home/ops/.ssh
+chmod 700 /home/ops/.ssh
+chmod 600 /home/ops/.ssh/authorized_keys
+
+# Allow sudo without password
+echo "ops ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Harden SSH settings
+sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+systemctl restart sshd
+"""
+
+# Export private key so you can SSH later
+pulumi.export("key_pair_name", key_pair.key_name)
+pulumi.export("private_key_material", private_key_pem.decode())
+
+
+#  Security Groups
+#  Public Security Group (for public instance)
+bastion_sg = ec2.SecurityGroup(
+    "bastion-sg",
+    vpc_id=vpc.id,
+    description="Security group for public EC2 instance",
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": 22,
+            "to_port": 22,
+            "cidr_blocks": ["0.0.0.0/0"],
+            "description": "SSH access",
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 80,
+            "to_port": 80,
+            "cidr_blocks": ["0.0.0.0/0"],
+            "description": "HTTP access",
+        },
+    ],
+    egress=[
+        {"protocol": "-1", "from_port": 0, "to_port": 0, "cidr_blocks": ["0.0.0.0/0"]}
+    ],
+    tags={"Name": "bastion-sg", "Environment": "dev"},
+)
+
+
+# AMI
+ami = ec2.get_ami(
+    most_recent=True,
+    owners=["amazon"],
+    filters=[{"name": "name", "values": ["amzn2-ami-hvm-*-x86_64-gp2"]}],
+).id
+
+# Create EC2 Instance in Public Subnet
+public_instance = ec2.Instance(
+    "bastion-instance",
+    ami=ami,
+    instance_type=instance_type,
+    subnet_id=public_subnet.id,
+    vpc_security_group_ids=bastion_sg.id,
+    associate_public_ip_address=True,
+    key_name=key_pair.key_name,
+    user_data=user_data,
+    tags={"Name": "bastion-instance", "Environment": "dev", "Tier": "public"},
+)
+
+
+pulumi.export("bastionPublicIp", public_instance.public_ip)
+
+
+# Task 3 — Private EC2 Instance
+
+# Private EC2 Security Group
+app_sg = ec2.SecurityGroup(
+    "app_sg ",
+    vpc_id=vpc.id,
+    description="Allow SSH only from Bastion Host",
+    ingress=[
+        {
+            "protocol": "tcp",
+            "from_port": 22,
+            "to_port": 22,
+            "cidr_blocks": ["0.0.0.0/0"],
+            "description": "SSH access",
+            # allow SSH only from bastion SG
+            "security_groups": [bastion_sg.id],
+        },
+        {
+            "protocol": "tcp",
+            "from_port": 80,
+            "to_port": 80,
+            "cidr_blocks": ["0.0.0.0/0"],
+            "description": "SSH access from Bastion",
+        },
+    ],
+    egress=[
+        {"protocol": "-1", "from_port": 0, "to_port": 0, "cidr_blocks": ["0.0.0.0/0"]}
+    ],
+    tags={"Name": "app-sg", "Environment": "dev"},
+    opts=pulumi.ResourceOptions(depends_on=[bastion_sg]),
+)
+
+# Create EC2 Instance in Private Subnet
+private_instance = ec2.Instance(
+    "private-instance",
+    ami=ami,
+    instance_type=instance_type,
+    subnet_id=private_subnet.id,
+    vpc_security_group_ids=[app_sg.id],
+    associate_public_ip_address=False,
+    key_name=key_pair.key_name,
+    tags={"Name": "private-instance", "Environment": "dev", "Tier": "private"},
+    opts=pulumi.ResourceOptions(depends_on=[public_instance]),
+)
+
+pulumi.export("privateInstancePrivateIp", private_instance.id)
